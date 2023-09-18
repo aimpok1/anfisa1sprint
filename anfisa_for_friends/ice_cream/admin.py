d727a4630878b2b26d05e64f4c032669c8af410f
Константин Волошин <aimpokwot@mail.ru>
2023-09-18 10:36:08 +0300

    sprint 4 first week

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+# Из модуля models импортируем модель Category...
+from .models import Category
+from .models import Topping
+from .models import Wrapper
+from .models import IceCream
+
+# admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+# admin.site.register(IceCream)
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+
+admin.site.register(IceCream, IceCreamAdmin)
+
+
+class IceCreamInline(admin.TabularInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
